44cce51ed34a98eb1f638d12b7048d085961195b
Bingo <chenbingo2017@gmail.com>
2019-05-23 09:10:17 +0800

    Update pom

--- ms-security-demo/src/main/java/com/bingo/security/MsSecurityDemoApplication.java
+++ ms-security-demo/src/main/java/com/bingo/security/MsSecurityDemoApplication.java
@@ -2,8 +2,11 @@ package com.bingo.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MsSecurityDemoApplication {
 
 
@@ -11,4 +14,8 @@ public class MsSecurityDemoApplication {
         SpringApplication.run(MsSecurityDemoApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security";
+    }
 }
